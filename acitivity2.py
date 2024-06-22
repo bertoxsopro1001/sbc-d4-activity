901e1f80894eafe8ed96ff2409964c53093d95fb
Bertox- <146450586+bertoxsopro1001@users.noreply.github.com>
2024-06-22 00:05:15 -0700

    acitivity2.py

--- acitivity2.py
+++ acitivity2.py
@@ -7,7 +7,7 @@ while True:
     # User input
     Player_1 = input("Enter 0 - fold, 1 - unfold : ")
    #input validation
-    if  Player_1.isdigit() & Player_1 in ['0', '1']:
+    if  Player_1.isdigit() and Player_1 in ['0', '1']:
          Player_1 = int(Player_1)
          print(f"Player 1 - {text_convert[Player_1]}\nComputer 1 - {text_convert[computer_1]}\nComputer 2 - {text_convert[computer_2]}\n"
           f"{'Player 1 win' if (computer_1 != Player_1) & (computer_2 != Player_1) else 'Computer 1 win' if (Player_1 != computer_1) & (computer_2 == Player_1) else 'Computer 2 win' if (Player_1 != computer_2) & (computer_1 == Player_1) else 'Draw'}")
