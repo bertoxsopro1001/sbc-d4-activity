b117351a019f97280b31d12efa443e92d0aff4f5
Bertox- <146450586+bertoxsopro1001@users.noreply.github.com>
2024-06-21 23:59:57 -0700

    acitivity2.py

--- acitivity2.py
+++ acitivity2.py
@@ -1,29 +1,18 @@
-#library
 from random import randint
-#randint loop 2 times
-Random = [randint(0,1) for i in range(2)]
+while True:
+    # Dictionary to convert number to word
+    text_convert = {0: "fold", 1: "unfold"}
+    # Assign random choices for computers
+    computer_1, computer_2 = [randint(0, 1) for _ in range(2)]
+    # User input
+    Player_1 = input("Enter 0 - fold, 1 - unfold : ")
+   #input validation
+    if  Player_1.isdigit() & Player_1 in ['0', '1']:
+         Player_1 = int(Player_1)
+         print(f"Player 1 - {text_convert[Player_1]}\nComputer 1 - {text_convert[computer_1]}\nComputer 2 - {text_convert[computer_2]}\n"
+          f"{'Player 1 win' if (computer_1 != Player_1) & (computer_2 != Player_1) else 'Computer 1 win' if (Player_1 != computer_1) & (computer_2 == Player_1) else 'Computer 2 win' if (Player_1 != computer_2) & (computer_1 == Player_1) else 'Draw'}")
+         
+    else: print("0 for fold, 1 for unfold" if Player_1.isalpha() else "Invalid input")
 
-#use  the number as a key to be easy to identify the value  nested dictionary
-choice = {"digit":{"fold" : 0,"unfold":1}, "number" :{0:"fold",1:"unfold"}}
 
-#assinging cp 1 and cp2 for randint
-computer_1,computer_2 = Random
-
-Player_1 = input("Enter Choice: Fold - Unfold ").lower()
-
-#validation
-if Player_1.isalpha():
-   #using boolean value for validate the winner
-   print(f" \n Player1 : {Player_1} \n Computer1 : {choice['number'][computer_1]} \n Computer2 {choice['number'][computer_2]}")
-   GameResult = (
-    "Player 1 wins" if (computer_1 * computer_2 == 0) * (choice["digit"][Player_1] == 1)
-    else "Player 1 wins" if (computer_1 * computer_2 == 1) * (choice["digit"][Player_1] == 0)
-    else "Computer 2 wins" if (computer_1 * choice["digit"][Player_1] == 0) * (computer_2 == 1)
-    else "Computer 2 wins" if (computer_1 * choice["digit"][Player_1] == 1) * (computer_2 == 0)
-    else "Computer 1 wins" if (computer_2 * choice["digit"][Player_1] == 0) * (computer_1 == 1)
-    else "Computer 2 wins" if (computer_2 * choice["digit"][Player_1] == 1) * (computer_1 == 0)
-    else "Draw"
-)
-   
-   print(GameResult)
-
+  
